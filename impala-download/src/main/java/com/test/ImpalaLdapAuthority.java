91e89b355fa5b31e5c062103443bce843b0785a3
nzh9900 <nzh9900@163.com>
2024-04-11 18:42:49 +0800

    feature: impala read

--- impala-download/src/main/java/com/test/ImpalaLdapAuthority.java
+++ impala-download/src/main/java/com/test/ImpalaLdapAuthority.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.sql.*;
 
 /**
@@ -10,24 +12,42 @@ import java.sql.*;
  * @Version 1.0
  **/
 public class ImpalaLdapAuthority {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException, UnsupportedEncodingException {
         handle();
     }
 
-    private static void handle() throws ClassNotFoundException, SQLException {
+    private static void handle() throws ClassNotFoundException, SQLException, InterruptedException, UnsupportedEncodingException {
         String driver = "com.cloudera.impala.jdbc41.Driver";
-        String jdbcUrl = "jdbc:impala://xxxxxxx:21052;AuthMech=3";
+        String jdbcUrl = "jdbc:impala://node21.test.com:21052;AuthMech=3";
         String username = "xxx";
-        String password = "xxxxx";
+        String password = "xxx";
         Class.forName(driver);
         try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
              Statement statement = connection.createStatement()) {
-            statement.execute("select * from default.null_test");
+            statement.execute("select * from default.test_abcx limit 10");
             ResultSet resultSet = statement.getResultSet();
-            System.out.println(resultSet.getMetaData().getColumnCount());
+            int columnCount = resultSet.getMetaData().getColumnCount();
+            System.out.println(columnCount);
             System.out.println(resultSet.getMetaData().getColumnTypeName(1));
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            StringBuffer buffer1 = new StringBuffer();
+            for (int i = 1; i <= columnCount; i++) {
+                buffer1.append(metaData.getColumnName(i)).append(",");
+            }
+            System.out.println(buffer1);
+
+
             while (resultSet.next()) {
-                System.out.println(resultSet.getString(1));
+                StringBuffer buffer = new StringBuffer();
+                for (int i = 1; i <= columnCount; i++) {
+                    String record = resultSet.getString(i);
+                    if (record!=null){
+                        record = new String(record.getBytes(Charset.forName("UTF-8")), "GBK");
+                    }
+                    buffer.append(record).append(",");
+                }
+                System.out.println(buffer);
+                Thread.sleep(1000);
             }
         }
 
